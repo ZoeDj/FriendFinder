01d5467b5b99cc01421d40aebe3e17388543c168
Zoe Dj <studio3fun@gmail.com>
2019-08-09 01:15:05 -0400

    friend-finder

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+var friendsList = require('../data/friends.js')
+
+module.exports = function(app){
+
+    app.get('/api/friends', function (req, res){
+    res.json(friendsList);
+    });
+
+
+app.post('/api/friends', function (req, res){
+    var newFriend= req.body
+    friendsList.push(newFriend);
+    res.json(true);
+})
+
+}
